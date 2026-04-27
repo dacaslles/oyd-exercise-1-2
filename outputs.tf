97cc057adc61e8430d974f75e357389ad0149955
dacaslles <dacaslles@gmail.com>
2026-04-26 20:07:15 -0600

    output file corrected

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,10 @@
 output "bucket_name" {
- value = aws_s3_bucket.main.bucket
+ value          = aws_s3_bucket.main.bucket
+ description    = "Nombre del bucket creado"
 }
 
 
 output "bucket_arn" {
- value = aws_s3_bucket.main.arn
+ value          = aws_s3_bucket.main.arn
+ description    = "Identificador del bucket creado"
 }
